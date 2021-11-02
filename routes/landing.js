1ac0311b247f636fe0bc233e393f7b749fc45a78
Vincelee78 <vincelee7778@gmail.com>
2021-11-02 07:48:30 +0000

    updated hands on

--- routes/landing.js
+++ routes/landing.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const {Product}=require('../models')
+
 
 router.get('/',(req,res)=>{
     res.render('landing/index')
@@ -17,5 +19,14 @@ router.get('/game/posters', (req,res)=>{
 router.get('/movie/posters', (req,res)=>{
     res.render('landing/movie')
 })
+
+router.get('/a', async (req,res)=>{
+    // #2 - fetch all the products (ie, SELECT * from products)
+    let products = await Product.collection().fetch();
+    res.render('products/index', {
+        'products': products.toJSON() // #3 - convert collection to JSON
+    })
+})
+
 
 module.exports=router;
