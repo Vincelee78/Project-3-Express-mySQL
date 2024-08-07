11ac5e02bcb0e5af267eaa7945909d710c4760bb
Vincelee78 <vincelee7778@gmail.com>
2024-08-07 19:34:55 +0000

    Your meaningful commit message

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -4,6 +4,7 @@ const knex = require('knex')({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT || 5432,
     'host': process.env.DB_HOST,
     'ssl': {
       'rejectUnauthorized': false
