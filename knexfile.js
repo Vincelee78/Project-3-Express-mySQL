dad11e069e41c3c3990e2d58a2cf1d998951b28d
Vincelee78 <vincelee7778@gmail.com>
2024-08-07 12:42:36 +0000

    Add knexfile.js configuration

---
+++ knexfile.js
@@ -0,0 +1,37 @@
+// knexfile.js
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+};
